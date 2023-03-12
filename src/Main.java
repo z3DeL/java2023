6357fd3ac911c2ed31cc388fcf05987f86e5fa54
M <dander0701@gmail.com>
2023-03-12 15:23:54 +0500

    1.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String name = scanner.nextLine();
+        System.out.println("Привет " + name);
+        int angle = 0;
+        System.out.println("Вывожу для вас таблицу синусов");
+        for (int i = 0; i < 2; i++){
+            for (int j=0; j<5; j++){
+                double rad = angle * Math.PI / 180;
+                String result = String.format("%.2f", Math.sin(rad ));
+                System.out.print("sin(" + angle + "\u00B0) = " + result + " ");
+            angle +=5;}
+            System.out.print("\n");
+        }
     }
 }
